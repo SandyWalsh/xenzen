603740773f0d929aea6edc2153ceefabe7f5df15
Sandy Walsh <sandy@sandywalsh.com>
2014-05-02 01:59:43 +0000

    scp

--- xenzen/dabble.py
+++ xenzen/dabble.py
@@ -1,9 +1,18 @@
 import git
+import paramiko
+import scp
 import XenAPI
 
 import json
 import os
 import sys
+
+def ssh_run(ssh, cmd):
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    stdin.close()
+    for line in stdout.read().splitlines():
+        print line
+
 
 with open(sys.argv[1]) as f:
     config = json.load(f)
@@ -24,8 +33,22 @@ print "Working directory:", work_dir
 os.chdir(work_dir)
 
 # Grab Nova code ...
-repo_dir = config['nova_repo']
-print "Cloning:", repo_dir
-repo = git.Git().clone(repo_dir)
+if not os.path.isdir('nova'):
+    repo_dir = config['nova_repo']
+    print "Cloning:", repo_dir
+    repo = git.Git().clone(repo_dir)
+else:
+    print "Using existing nova repo."
 
+# scp the xen plugins from nova repo to xenserver ...
+ssh = paramiko.SSHClient()
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+scp_ip = config['scp_ip']
+scp_username = config['scp_username']
+scp_password = config['scp_password']
+ssh.connect(scp_ip, username=scp_username, password=scp_password)
+client = scp.SCPClient(ssh.get_transport())
 
+ssh_run(ssh, "mkdir /root/temp")
+client.put("/opt/stack/xenzen", remote_path='/root/temp', recursive=True)
